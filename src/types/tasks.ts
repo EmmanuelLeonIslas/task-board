64237f4309d7017d46c684c4b7103e6a863eadef
Emmanuel León <cn-emmanuel@hotmail.com>
2026-03-11 22:57:58 -0600

    Adding API config

--- src/types/tasks.ts
+++ src/types/tasks.ts
@@ -1,12 +1,13 @@
 export type TaskStatus = "pending" | "in_progress" | "completed";
 
 export interface Task {
-  readonly id: string;
+  readonly _id: string;
   title: string;
   status: TaskStatus;
-  readonly createdAt: Date;
+  readonly createdAt: Date | string; //Puede ser de string del backend
+  readonly updatedAt?: Date | string; //Lo agrega MongoDB
 }
 
 export type CreateTaskDTO = Pick<Task, "title">;
-export type UpdateTaskDTO = Partial<Omit<Task, "createdAt">> & Pick<Task, "id">;
-export type DeleteTaskDTO = Pick<Task, "id">;
+export type UpdateTaskDTO = Partial<Omit<Task, "createdAt" | "updatedAt">> & Pick<Task, "_id">;
+export type DeleteTaskDTO = Pick<Task, "_id">;
